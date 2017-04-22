840d8efdfab7c8cd4f9c03236b307abec0641478
DJWOMS <l2maximum@mail.ru>
2017-04-22 13:40:46 +0300

    Start

--- metro/settings.py
+++ metro/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'metro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'metro',
+        'USER': 'root',
+        'PASSWORD': 'Djwoms18',
+        'HOST': 'localhost',
     }
 }
 
@@ -103,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'RU-ru'
 
 TIME_ZONE = 'UTC'
 
