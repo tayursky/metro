63e0345b27ac8ce0397c16c3a4f4bf9a4066c841
DJWOMS <l2maximum@mail.ru>
2017-04-29 10:44:53 +0300

    Страницы добавления

--- myclient/urls.py
+++ myclient/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     url(r'^my-client/$', views.my_client, name='my_client'),
     url(r'^my-object/$', views.my_object, name='my_object'),
     url(r'^my-task/$', views.my_task, name='my_task'),
+    url(r'^add-client/$', views.add_client, name='add_client'),
+    url(r'^add-object/$', views.add_object, name='add_object'),
+    url(r'^add-task/$', views.add_task, name='add_task'),
 ]
--- myclient/views.py
+++ myclient/views.py
@@ -8,10 +8,35 @@ from django.contrib.auth.decorators import login_required
 def my_task(request):
     return render(request, 'myclient/my-task.html')
 
+# Добавления задания
+
+
+@login_required
+def add_task(request):
+    return render(request, 'myclient/add-task.html')
+
+# Добавления клиента
+
+
+@login_required
+def add_client(request):
+    return render(request, 'myclient/add-client.html')
+
+# Мои клиенты
+
 
 @login_required
 def my_client(request):
     return render(request, 'myclient/my-client.html')
+
+# Добавление объекта
+
+
+@login_required
+def add_object(request):
+    return render(request, 'myclient/add-object.html')
+
+# Мои объекты
 
 
 @login_required
