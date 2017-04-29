710470c605215cc97f388c0a1361199048114b53
DJWOMS <l2maximum@mail.ru>
2017-04-29 10:25:01 +0300

    Templates

--- myclient/urls.py
+++ myclient/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^my-client/$', views.my_client, name='my_client'),
+    url(r'^my-object/$', views.my_object, name='my_object'),
     url(r'^my-task/$', views.my_task, name='my_task'),
 ]
--- myclient/views.py
+++ myclient/views.py
@@ -7,3 +7,13 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def my_task(request):
     return render(request, 'myclient/my-task.html')
+
+
+@login_required
+def my_client(request):
+    return render(request, 'myclient/my-client.html')
+
+
+@login_required
+def my_object(request):
+    return render(request, 'myclient/my-object.html')
