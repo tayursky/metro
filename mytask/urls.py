fbd44930f9bc285ae097face62c0629798c9f902
DJWOMS <l2maximum@mail.ru>
2017-05-05 05:41:34 +0300

    Admin

--- mytask/urls.py
+++ mytask/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^my-task/$', views.my_task, name='my_task'),
+    url(r'^$', views.my_task, name='my_task'),
     url(r'^add-task/$', views.add_task, name='add_task'),
 ]
