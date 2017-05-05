3b882747da3e7a03460ca2e2774af2a09cb0b4b2
DJWOMS <l2maximum@mail.ru>
2017-05-05 05:09:50 +0300

    Home

--- metro/urls.py
+++ metro/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
+from . import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^', views.home, name="home"),
     url(r'^login/client/', include('myclient.urls')),
     url(r'^login/object/', include('myobject.urls')),
     url(r'^login/task/', include('mytask.urls')),
