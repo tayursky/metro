e8e7f319451960280dd813da51c5770d32745509
axcel <axcel.github@gmail.com>
2017-06-25 16:32:49 +0500

    some change SearchObjFullFront form

--- metro_site/forms.py
+++ metro_site/forms.py
@@ -1,14 +1,23 @@
 from django import forms
-from myobject.models import MyObject
+
+from myclient.models import Naznach
 
 
 class SearchObjectFront(forms.Form):
     ''' Форма поиска объекта для клиента '''
-    search = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Номер объекта'}), max_length=30, error_messages={'required': ''})
+    search = forms.CharField(label='',
+                             widget=forms.TextInput(
+                                 attrs={'placeholder': 'Номер объекта'}),
+                             max_length=30, error_messages={'required': ''})
+
 
 class SearchMetroFront(forms.Form):
     ''' Форма поиска метро для клиента '''
-    search = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Станция метро'}), max_length=30, error_messages={'required': ''})
+    search = forms.CharField(label='',
+                             widget=forms.TextInput(
+                                 attrs={'placeholder': 'Станция метро'}),
+                             max_length=30, error_messages={'required': ''})
+
 
 class SearchObjFullFront(forms.Form):
     ''' Форма поиска объекта по параметрам '''
@@ -16,7 +25,7 @@ class SearchObjFullFront(forms.Form):
         ('small', 'Маленькое до 20 кв.м.'),
         ('middle', 'Среднее от 20 до 80 кв.м.'),
         ('large', 'Большое до 80 кв.м.')
-        )
+    )
     REGION = (
         ('1', 'Центер и ТТК'),
         ('2', 'Север'),
@@ -24,6 +33,22 @@ class SearchObjFullFront(forms.Form):
         ('4', 'Восток'),
         ('5', 'Запад'),
         ('6', 'Область')
-        )
-    premise = forms.ChoiceField(label='ПОМЕЩЕНИЕ', widget=forms.RadioSelect, choices=PREMISE)
-    region = forms.ChoiceField(label='РАЙОНЫ МОСКВЫ', widget=forms.RadioSelect, choices=REGION)
+    )
+    PRICE = (
+        ('1', '50000'),
+        ('2', '100000'),
+        ('3', '250000'),
+        ('4', '500000'),
+        ('5', '1000000'),
+        ('6', '10000000'),
+    )
+
+    premise = forms.ChoiceField(label='ПОМЕЩЕНИЕ',
+                                widget=forms.RadioSelect, choices=PREMISE)
+    region = forms.ChoiceField(label='РАЙОНЫ МОСКВЫ',
+                               widget=forms.RadioSelect, choices=REGION)
+    price = forms.ChoiceField(label='АРЕНДА В МЕСЯЦ',
+                              widget=forms.RadioSelect, choices=PRICE)
+    naznach = forms.ModelChoiceField(queryset=Naznach.objects.all(),
+                                     label='ВИД ДЕЯТЕЛЬНОСТИ',
+                                     widget=forms.RadioSelect)
