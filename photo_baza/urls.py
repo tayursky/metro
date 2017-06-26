02ec6a42be26e97a90bade2a2fe5254f6ad8631d
olegJF <jf2@ua.fm>
2017-06-26 20:02:36 +0300

    added upload photo from URL

--- photo_baza/urls.py
+++ photo_baza/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
-from .views import FileFieldView
+from .views import *
 
 urlpatterns = [
     url(r'^upload/$', FileFieldView.as_view()),
-##    url(r'^upload_url/$', UploadURLView.as_view(), name="upload-url"),
+    url(r'^upload_url/$', UploadURLView.as_view(), name="upload-url"),
 ##    url(r'^show/(?P<pk>\d+)/$', UploadDetailView.as_view(), name="upload-detail"),
 ]
--- photo_baza/views.py
+++ photo_baza/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.views.generic.edit import FormView
+from django.views.generic import DetailView
 from .forms import FileFieldForm
 from .models import Photo
+from .forms import UploadURLForm
+from .utils import *
+from django.contrib.messages.views import SuccessMessageMixin
+
 
 class FileFieldView(FormView):
     form_class = FileFieldForm
@@ -20,4 +25,46 @@ class FileFieldView(FormView):
             return self.form_valid(form)
         else:
             return self.form_invalid(form)
+
+class UploadURLView(SuccessMessageMixin, FormView):
+    form_class = UploadURLForm
+    template_name = "photo_baza/upload_from_url.html"
+    success_url = '/photo/upload_url/'
+
+##    def get_success_url(self):
+##        return reverse("upload-detail", args=[self.image_from_url.pk, ])
+
+    def form_valid(self, form):
+        def _invalidate(msg):
+            form.errors['url'] = [msg, ]
+            return super(UploadURLView, self).form_invalid(form)
+
+
+        url = form.data['url']
+        messege_id = form.data['station']
+        domain, path = split_url(url)
+        filename = get_url_tail(path)
+        
+        fobject = retrieve_image(url)
+        if not fobject:
+            return _invalidate("Ошибка url-адреса")
+        
+        pil_image = Image.open(fobject[0]) 
+        if pil_image.format.lower() not in VALID_IMAGE_EXTENSIONS:
+            return _invalidate("Файл не является изображением")
+        
+
+        django_file = pil_to_django(pil_image)
+
+        self.uploaded_image = Photo(station_id=messege_id)
+        self.uploaded_image.image.save(filename, django_file)
+        self.uploaded_image.save()
+##        success_message ='Фотография добавлена'
+
+        return super(UploadURLView, self).form_valid(form)
+
+class UploadDetailView(DetailView):
+    model = Photo
+    context_object_name = "image"
+    template_name = "photo_baza/detail.html"
 
