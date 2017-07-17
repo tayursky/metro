90eb47f83f6d7ed0e4806bbedd4a144fd0829bf3
DJWOMS <l2maximum@mail.ru>
2017-07-17 06:25:01 +0300

    Search v0.6.12

--- metro_site/views.py
+++ metro_site/views.py
@@ -15,7 +15,6 @@ def home(request):
     if request.method == "POST":
         form = SearchObjFullFront(request.POST)
         if form.is_valid():
-            myobjs = []
             form_price = form.cleaned_data['price']
             okrug = form.cleaned_data['okrug']
             area = form.cleaned_data['area_range']
@@ -30,38 +29,31 @@ def home(request):
                 okrug = Okrug.objects.all()
 
             if (not area) and (not naznach):
-                for st in StancMetro.objects.filter(okrug__in=okrug).distinct():
-                    myobjs.append(MyObject.objects.filter(
+                myobjs = MyObject.objects.filter(
                         price__lte=price,
-                        okrug__in=okrug,
-                        station_one=st
-                    ).order_by("station_one"))
+                        okrug__in=okrug
+                        ).order_by("station_one").distinct()
             elif not area:
-                for st in StancMetro.objects.filter(okrug__in=okrug).distinct():
-                    myobjs.append(MyObject.objects.filter(
+                myobjs = MyObject.objects.filter(
                         naznach=form.cleaned_data['naznach'],
                         price__lte=price,
-                        okrug__in=okrug,
-                        station_one=st
-                    ).order_by("station_one"))
+                        okrug__in=okrug
+                    ).order_by("station_one").distinct()
             elif not naznach:
-                for st in StancMetro.objects.filter(okrug__in=okrug).distinct():
-                    myobjs.append(MyObject.objects.filter(
+                myobjs = MyObject.objects.filter(
                         area_range=area,
                         price__lte=price,
-                        okrug__in=okrug,
-                        station_one=st
-                    ).order_by("station_one"))
+                        okrug__in=okrug
+                    ).order_by("station_one").distinct()
             else:
-                for st in StancMetro.objects.filter(okrug__in=okrug).distinct():
-                    myobjs.append(MyObject.objects.filter(
+                myobjs = MyObject.objects.filter(
                         naznach=form.cleaned_data['naznach'],
                         area_range=area,
                         price__lte=price,
-                        okrug__in=okrug,
-                        station_one=st
-                    ).order_by("station_one"))
-            context = {'myobjs': myobjs, 'form': form}
+                        okrug__in=okrug
+                    ).order_by("station_one").distinct()
+            form = SearchObjFullFront(data=request.POST)
+            context = {'mytest': myobjs, 'form': form}
             return render(request, 'site/search.html', context)
 
     context = {'myobj': myobj, 'form': form}
@@ -118,15 +110,18 @@ def obj_single(request, pk):
 
 def new_obj(request):
     '''Новые объекты'''
-    myobjs = []
-    myobjs.append(MyObject.objects.order_by('-id')[:16])
-    context = {'myobjs': myobjs}
+    myobjs = MyObject.objects.all().order_by("station_one")[:16]
+    form = SearchObjFullFront()
+    context = {'mytest': myobjs, 'form': form}
     return render(request, 'site/search.html', context)
 
 
 def under(request):
     '''Объекты подземки'''
-    myobjs = []
-    myobjs.append(MyObject.objects.filter(typeobj=4))
-    context = {'myobjs': myobjs}
+    myobjs = MyObject.objects.filter(typeobj=4).order_by("station_one")
+    form = SearchObjFullFront()
+    context = {'mytest': myobjs, 'form': form}
     return render(request, 'site/search.html', context)
+
+def serch_test(request):
+    pass
